24ea7c92863e508462f2f94afb369a03afc77d68
Marstheway <kentsoft@gmail.com>
2025-10-14 11:44:41 +0800

    更广泛的newtab

--- ui/v2.5/src/components/Shared/GridCard/GridCard.tsx
+++ ui/v2.5/src/components/Shared/GridCard/GridCard.tsx
@@ -217,7 +217,7 @@ export const GridCard: React.FC<ICardProps> = (props: ICardProps) => {
           to={props.url}
           className={props.linkClassName}
           onClick={handleImageClick}
-          target={props.url.startsWith("/galleries/") || props.url.startsWith("/scenes/") ? "_blank" : undefined}
+          target="_blank"
         >
           {props.image}
         </Link>
@@ -226,7 +226,7 @@ export const GridCard: React.FC<ICardProps> = (props: ICardProps) => {
       </div>
       {maybeRenderInteractiveHeatmap()}
       <div className="card-section">
-        <Link to={props.url} onClick={handleImageClick} target={props.url.startsWith("/galleries/") || props.url.startsWith("/scenes/") ? "_blank" : undefined}>
+        <Link to={props.url} onClick={handleImageClick} target="_blank">
           <h5 className="card-section-title flex-aligned">
             {props.pretitleIcon}
             <TruncatedText text={props.title} lineCount={2} />
