f6da6165f9c7b5f27014666b9f7d15d3df86bb4c
Marstheway <kentsoft@gmail.com>
2025-10-16 14:12:39 +0800

    Reapply "平板禁用mobileUi"
    This reverts commit aca7b5db7adc81c33e0bea82005a355c2e3ce497.

--- ui/v2.5/src/components/ScenePlayer/ScenePlayer.tsx
+++ ui/v2.5/src/components/ScenePlayer/ScenePlayer.tsx
@@ -644,7 +644,7 @@ export const ScenePlayer: React.FC<IScenePlayerProps> = PatchComponent(
           disabled: true,
         },
       };
-      if (!isSafari) {
+      if (!isSafari && ScreenUtils.isMobile()) {
         player.mobileUi(mobileUiOptions);
       }
 
